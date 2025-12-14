ccd85ddfb99ab1e63ad5b8ee5706a75bc8fea73e
A Lalith <lalithsep394@gmail.com>
2025-12-14 18:02:01 +0530

    Added put and delete to exp5

--- exp5/controllers/contactController.js
+++ exp5/controllers/contactController.js
@@ -18,3 +18,32 @@ exports.createContact = async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 };
+
+exports.updateContact = async (req, res) => {
+  try {
+    const {name, email, phone} = req.body;
+    const updatedContact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new:true
+      }
+    )
+    if(!updatedContact) return res.status(404).json({error: "Contact to update not found!"});
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({message: "Internal server error!!"});
+  }
+}
+
+exports.deleteContact = async(req, res) => {
+  try {
+    const deletedContact = await Contact.findByIdAndDelete(req.params.id);
+    if (!deletedContact) return res.status(404).json("Contact Not found");
+    res.status(200).json({message: "Deleted successfully"});
+    
+  } catch (error) {
+    res.status(500).json({message:"Internal server error"});
+  }
+}
--- exp5/routes/contactRoutes.js
+++ exp5/routes/contactRoutes.js
@@ -4,5 +4,7 @@ const contactController = require('../controllers/contactController');
 
 router.get('/', contactController.getAllContacts);
 router.post('/', contactController.createContact);
+router.put('/:id', contactController.updateContact);
+router.delete('/:id', contactController.deleteContact);
 
 module.exports = router;
